c151d15771d387f1003b7e4dc902ff0a9b073d73
foodkin <jasonjeferson81@gmail.com>
2025-11-23 22:12:04 +0700

    More Mobile App

--- src/data/mobileProjects.js
+++ src/data/mobileProjects.js
@@ -4,7 +4,7 @@ const mobileProjects = [
     title: 'Absensi App',
     description: 'Attendance application with live tracking location.',
     image: '📱',
-    tags: ['Kotlin', 'Xml'],
+    tags: ['Kotlin', 'Xml', 'Android'],
     apk: '/downloads/Absensi.apk',
     github: 'https://github.com/foodkin/AbsensiApp'
   },
@@ -12,9 +12,17 @@ const mobileProjects = [
     title: 'Daily Gabut Quest',
     description: 'Gabut application for Gabut people who are confused about what to do.',
     image: '📱',
-    tags: ['Kotlin', 'Xml'],
+    tags: ['Kotlin', 'Xml', 'Android'],
     apk: '/downloads/Daily-Gabut-Quest.apk',
     github: 'https://github.com/foodkin/Daily_Gabut_Quest'
+  },
+    {
+    title: 'Math',
+    description: 'Application to test your basic math skills',
+    image: '📱',
+    tags: ['Kotlin', 'Xml', 'Android'],
+    apk: '/downloads/Math.apk',
+    github: 'https://github.com/foodkin/Math.git'
   }
 ];
 
